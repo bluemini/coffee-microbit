555048672206b62f7d73888445dfc93aa02c68bc
Nick Harvey <nick@bluemini.com>
2022-01-04 22:16:33 -0800

    plot graph is now a function

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
+let realY = 0
 let temp1 = 0
 let temp2 = 0
-let realY = 0
+let dir = 0
+let pinin = 0
+function plot (col: number, value: number) {
+    for (let index = 0; index <= 4; index++) {
+        realY = 4 - index
+        if (value > (index + 1) * 170) {
+            led.plot(col, realY)
+        } else {
+            led.unplot(col, realY)
+        }
+    }
+}
 basic.forever(function () {
     temp1 = pins.analogReadPin(AnalogPin.P0)
     temp2 = pins.analogReadPin(AnalogPin.P1)
-    serial.writeNumbers([temp1, temp2])
-    for (let index = 0; index <= 4; index++) {
-        realY = 4 - index
-        if (temp1 > (index + 1) * 170) {
-            led.plot(0, realY)
-        } else {
-            led.unplot(0, realY)
-        }
-        if (temp2 > (index + 1) * 170) {
-            led.plot(1, realY)
-        } else {
-            led.unplot(1, realY)
-        }
-    }
+    dir = input.acceleration(Dimension.X)
+    pinin = pins.digitalReadPin(DigitalPin.P8)
+    serial.writeNumbers([temp1, temp2, dir])
+    plot(0, temp1)
+    plot(1, temp2)
+    plot(2, dir)
     basic.pause(100)
 })
