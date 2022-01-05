387ee88a2e30d8e88eb38b1d78c8298939d16b26
Nick Harvey <nick@bluemini.com>
2022-01-04 21:30:38 -0800

    added graph on second pin input

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
 let temp1 = 0
 let temp2 = 0
+let realY = 0
 basic.forever(function () {
     temp1 = pins.analogReadPin(AnalogPin.P0)
     temp2 = pins.analogReadPin(AnalogPin.P1)
     serial.writeNumbers([temp1, temp2])
     for (let index = 0; index <= 4; index++) {
-        let realY = 4 - index
-        if (temp1 > (index + 1) * 200) {
+        realY = 4 - index
+        if (temp1 > (index + 1) * 170) {
             led.plot(0, realY)
         } else {
             led.unplot(0, realY)
+        }
+        if (temp2 > (index + 1) * 170) {
+            led.plot(1, realY)
+        } else {
+            led.unplot(1, realY)
         }
     }
     basic.pause(100)
